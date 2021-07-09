680ce8b9a5183b72ca9609ee164aaf2c3d2e1ec3
Sheharyar Naseer <hello@sheharyar.me>
2021-07-09 22:05:31 +0500

    Create EmbedHandler behaviour, and use it for TableEmbeds

---
+++ lib/slab/tandem/embed_handler.ex
@@ -0,0 +1,9 @@
+defmodule Slab.Tandem.EmbedHandler do
+  @type t :: module()
+  @type embed :: map()
+
+  @callback name() :: binary()
+  @callback compose(embed(), embed(), keep_nil? :: boolean()) :: embed()
+  @callback transform(embed(), embed(), priority? :: boolean()) :: embed()
+  @callback invert(embed(), embed()) :: embed()
+end
