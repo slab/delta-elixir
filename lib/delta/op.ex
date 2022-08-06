d10b8766543c45ab7bee6ed1ea226bf9801d9909
Vincent Herr <vherr2@illinois.edu>
2022-08-05 20:54:39 -0700

    Adds typespecs

--- lib/delta/op.ex
+++ lib/delta/op.ex
@@ -1,7 +1,51 @@
 defmodule Delta.Op do
   alias Delta.Attr
   alias Delta.Utils
+  alias Delta.EmbedHandler
 
+  @type t :: insert_op | retain_op | delete_op
+
+  @typedoc """
+  Stand-in type while operators are keyed with String.t() instead of Atom.t()
+
+  `%{insert: bitstring() | EmbedHandler.embed(), ...}`
+  """
+  @type insert_op :: %{required(insert_key) => insert_val, optional(attributes) => attributes_val}
+  @typep insert_key :: String.t()
+  @typep insert_val :: bitstring() | EmbedHandler.embed()
+
+  @typedoc """
+  Stand-in type while operators are keyed with String.t() instead of Atom.t()
+
+  `%{retain: pos_integer() | EmbedHandler.embed()}`
+  """
+  @type retain_op :: %{required(retain_key) => retain_val, optional(attributes) => attributes_val}
+  @typep retain_key :: String.t()
+  @typep retain_val :: pos_integer() | EmbedHandler.embed()
+
+
+  @typedoc """
+  Stand-in type while operators are keyed with String.t() instead of Atom.t()
+
+  `%{delete: pos_integer()}`
+  """
+  @type delete_op :: %{required(delete_key) => pos_integer}
+  @typep delete_key :: String.t()
+  @typep delete_val :: pos_integer()
+
+  @typedoc """
+  Stand-in type while operators are keyed with String.t() instead of Atom.t()
+  """
+  @type operation :: insert_key | retain_key | delete_key
+  @typep operation_val :: insert_val | retain_val | delete_val
+
+  @typedoc """
+  Stand-in type while attributes is keyed with String.t() instead of Atom.t()
+  """
+  @type attributes :: %{required(String.t()) => attributes_val}
+  @typep attributes_val :: map() | false
+
+  @spec new(action :: operation, value :: operation_val, attr :: attributes_val) :: t
   def new(action, value, attr \\ false)
 
   def new(action, value, %{} = attr) when map_size(attr) > 0 do
@@ -10,13 +54,20 @@ defmodule Delta.Op do
 
   def new(action, value, _attr), do: %{action => value}
 
+  @spec insert(value :: insert_val, attr :: attributes_val) :: insert_op
   def insert(value, attr \\ false), do: new("insert", value, attr)
+
+  @spec retain(value :: retain_val, attr :: attributes_val) :: retain_op
   def retain(value, attr \\ false), do: new("retain", value, attr)
+
+  @spec delete(value :: delete_val, attr :: attributes_val) :: delete_op
   def delete(value, attr \\ false), do: new("delete", value, attr)
 
+  @spec has_attributes?(any) :: boolean
   def has_attributes?(%{"attributes" => %{}}), do: true
   def has_attributes?(_), do: false
 
+  @spec type?(op :: t, action :: any, value_type :: any) :: boolean
   def type?(op, action, value_type \\ nil)
   def type?(%{} = op, action, nil) when is_map_key(op, action), do: true
   def type?(%{} = op, action, :map), do: is_map(op[action])
@@ -24,10 +75,16 @@ defmodule Delta.Op do
   def type?(%{} = op, action, :number), do: is_integer(op[action])
   def type?(%{}, _action, _value_type), do: false
 
+  @spec insert?(op :: t, type :: any) :: boolean
   def insert?(op, type \\ nil), do: type?(op, "insert", type)
+
+  @spec delete?(op :: t, type :: any) :: boolean
   def delete?(op, type \\ nil), do: type?(op, "delete", type)
+
+  @spec retain?(op :: t, type :: any) :: boolean
   def retain?(op, type \\ nil), do: type?(op, "retain", type)
 
+  @spec text_size(text :: binary) :: non_neg_integer
   def text_size(text) do
     text
     |> :unicode.characters_to_binary(:utf8, :utf16)
@@ -35,11 +92,13 @@ defmodule Delta.Op do
     |> div(2)
   end
 
+  @spec size(t) :: non_neg_integer
   def size(%{"insert" => text}) when is_binary(text), do: text_size(text)
   def size(%{"delete" => len}) when is_integer(len), do: len
   def size(%{"retain" => len}) when is_integer(len), do: len
   def size(_op), do: 1
 
+  @spec take(op :: t, length :: non_neg_integer, opts :: Keyword.t()) :: {t, t | boolean}
   def take(op, length, opts \\ [])
 
   def take(op = %{"insert" => embed}, _length, _opts) when not is_bitstring(embed) do
@@ -71,6 +130,7 @@ defmodule Delta.Op do
     end
   end
 
+  @spec compose(a :: t, b :: t) :: {t | false, t, t}
   def compose(a, b) do
     {op1, a, op2, b} = next(a, b)
 
@@ -108,6 +168,7 @@ defmodule Delta.Op do
     {composed, a, b}
   end
 
+  @spec transform(non_neg_integer, non_neg_integer, t, boolean) :: {non_neg_integer, non_neg_integer}
   def transform(offset, index, op, priority) when is_integer(index) do
     length = size(op)
 
@@ -118,6 +179,7 @@ defmodule Delta.Op do
     end
   end
 
+  @spec transform(a :: t, b :: t, priority :: boolean) :: {t | false, t, t}
   def transform(a, b, priority) do
     {op1, a, op2, b} = next(a, b)
 
@@ -152,6 +214,7 @@ defmodule Delta.Op do
     {transformed, a, b}
   end
 
+  @spec next(t, t) :: {t, t, t, t}
   defp next(a, b) do
     size = min(size(a), size(b))
     {op1, a} = take(a, size)
@@ -159,6 +222,7 @@ defmodule Delta.Op do
     {op1, a, op2, b}
   end
 
+  @spec take_partial(t, non_neg_integer, Keyword.t) :: {t, t}
   defp take_partial(op, 0, _opts), do: {insert("", op["attributes"]), op}
 
   defp take_partial(%{"insert" => text} = op, len, opts) do
@@ -202,6 +266,7 @@ defmodule Delta.Op do
     {retain(length, op["attributes"]), retain(full - length, op["attributes"])}
   end
 
+  @spec info(t) :: {String.t(), :number | :string | :map}
   defp info(op) do
     action =
       case op do
